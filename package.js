788f67dddf428f4de48baba35d8f0bdcbdb11339
mudauthendo2011-hub <mudauthendo2011@gmail.com>
2025-12-12 11:26:58 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,31 @@
+{
+  "name": "mt-rk",
+  "version": "1.0.0",
+  "description": "this bot was made by royal",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/mudauthendo2011-hub/MT-RK.git"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "type": "commonjs",
+  "bugs": {
+    "url": "https://github.com/mudauthendo2011-hub/MT-RK/issues"
+  },
+  "homepage": "https://github.com/mudauthendo2011-hub/MT-RK#readme",
+  "dependencies": {
+    "@adiwajshing/baileys": "^5.0.0",
+    "@hapi/boom": "^10.0.1",
+    "axios": "^1.13.2",
+    "chalk": "^5.6.2",
+    "dotenv": "^17.2.3",
+    "fs-extra": "^11.3.2",
+    "moment": "^2.29.4"
+  }
+  }
