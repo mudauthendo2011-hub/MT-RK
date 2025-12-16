c435fe4b4e193bf1a466a62bc664d69035edfb24
mudauthendo2011-hub <mudauthendo2011@gmail.com>
2025-12-16 16:42:07 +0200

    Update index.js

--- core /index.js
+++ core /index.js
@@ -1,44 +1,44 @@
-const fs = require('fs')
-const path = require('path')
-const rootDir = path.join(__dirname)
+const fs = require("fs")
+const path = require("path")
+const rootDir = __dirname
 const modules = {}
 
-var bin = path.join(__dirname, 'bin')
+const bin = path.join(__dirname, "bin")
 
 if (!fs.existsSync(bin)) {
   fs.mkdirSync(bin, { recursive: true })
 }
 
-global.bin = path.join(__dirname, 'bin')
-global.strd = path.join(__dirname, 'store')
+global.bin = bin
+global.strd = path.join(__dirname, "store")
 
-const configPath = path.join(__dirname, '..', 'config.js')
+const configPath = path.join(__dirname, "..", "config.js")
 modules.config = () => require(configPath)
 
 function processDir(dirPath) {
   const files = fs.readdirSync(dirPath)
-  
+
   files.forEach(file => {
     const filePath = path.join(dirPath, file)
     const stats = fs.statSync(filePath)
-    
+
     if (stats.isDirectory()) {
       processDir(filePath)
-    } else if (stats.isFile() && path.extname(file) === '.js' && file !== 'index.js') {
+    } else if (
+      stats.isFile() &&
+      path.extname(file) === ".js" &&
+      file !== "index.js"
+    ) {
       const mod = require(filePath)
-      
-      if (typeof mod === 'object') {
-        for (const key in mod) {
-          modules[key] = mod[key]
-        }
+
+      if (typeof mod === "object") {
+        Object.assign(modules, mod)
       } else {
-        const name = path.basename(file, '.js')
-        modules[name] = mod
+        modules[path.basename(file, ".js")] = mod
       }
     }
   })
 }
 
 processDir(rootDir)
-
 module.exports = modules
